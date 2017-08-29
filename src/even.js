8bea831c1447debae1995d7af55b35d52d32ce92
puer <vova@mytnik.net>
2017-08-29 18:29:02 +0300

    edit eslint warnings

--- src/even.js
+++ src/even.js
@@ -6,15 +6,15 @@ export const getName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${getName}!\n`);
 let i = 3;
 export const makeEvenGame = () => {
-  const curDigit = Math.floor(Math.random()*100);
+  const curDigit = Math.floor(Math.random() * 100);
   console.log(`Question: ${curDigit}`);
-  const userAnsw = readlineSync.question(`Your answer: `);
+  const userAnsw = readlineSync.question('Your answer: ');
   const checkAnsw = curDigit % 2 === 0 ? 'yes' : 'no';
   if (i === 1) {
     console.log(`Congratulations, ${getName}`);
   } else if (checkAnsw === userAnsw) {
-    i--;
-    console.log(`Correct!`);
+    i -= 1;
+    console.log('Correct!');
     return makeEvenGame();
   } else {
     return console.log(`${userAnsw} is wrong answer ;(. Correct answer was ${checkAnsw}.`);

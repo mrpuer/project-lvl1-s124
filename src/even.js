3e06a3b829fdd9685d8621f2997ac80d509bba25
puer <vova@mytnik.net>
2017-08-29 18:48:16 +0300

    fix eslint warms

--- src/even.js
+++ src/even.js
@@ -6,16 +6,17 @@ export const getName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${getName}!\n`);
 
 export const makeEvenGame = (i) => {
+  let count = i;
   const curDigit = Math.floor(Math.random() * 100);
   console.log(`Question: ${curDigit}`);
   const userAnsw = readlineSync.question('Your answer: ');
   const checkAnsw = curDigit % 2 === 0 ? 'yes' : 'no';
   if (i === 1) {
-    console.log(`Congratulations, ${getName}`);
+    return console.log(`Congratulations, ${getName}`);
   } else if (checkAnsw === userAnsw) {
     console.log('Correct!');
-    return makeEvenGame(i -= 1);
-  } else {
-    return console.log(`${userAnsw} is wrong answer ;(. Correct answer was ${checkAnsw}.`);
+    count -= 1;
+    return makeEvenGame(count);
   }
+  return console.log(`${userAnsw} is wrong answer ;(. Correct answer was ${checkAnsw}.`);
 };
